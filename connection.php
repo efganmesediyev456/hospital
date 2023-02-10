ee4a84dee3c50edb07b88c2db8693c0f89bb0fcf
Maryam <maryam@Maryams-MacBook-Pro.local>
2023-02-10 18:28:03 +0400

    first commit

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+
+    $database= new mysqli("localhost","root","root","hospital");
+    if ($database->connect_error){
+        die("Connection failed:  ".$database->connect_error);
+    }
+
+?>
